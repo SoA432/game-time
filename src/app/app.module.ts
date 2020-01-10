f9d5b68259801a8b20646b40769ff1697d500ecb
Yevhen_H <33688931+SoA432@users.noreply.github.com>
2020-01-10 16:55:08 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GamePlanetComponent } from './components/game-planet/game-planet.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamePlanetComponent,
+    HeaderComponent,
+    SliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/game-planet/game-planet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-planet',
+  templateUrl: './game-planet.component.html',
+  styleUrls: ['./game-planet.component.css']
+})
+export class GamePlanetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
