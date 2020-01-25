02eb8b8540050c5acdfdbe9dc6f2850e385a39b4
Yevhen_H <yevhen@darly.solutions>
2020-01-25 16:10:02 +0200

    implemented header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public isSearchHidden = true;
+  public searchControl: FormControl;
+  private destroy$ = new Subject();
+  @ViewChild('search', {static: false}) search: ElementRef;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.searchControl = new FormControl('');
+    this.searchControl.valueChanges.pipe(
+      takeUntil(this.destroy$),
+      debounceTime(400),
+      distinctUntilChanged(),
+      filter((val) => val)
+    ).subscribe((value: string) => {
+      console.log(value);
+    });
+  }
+
+  public showSearch(): void {
+    this.isSearchHidden = !this.isSearchHidden;
+    if (!this.isSearchHidden) {
+      setTimeout(() => this.search.nativeElement.focus(), 0);
+    }
+    console.log('show', this.isSearchHidden);
+  }
+
+  ngOnDestroy(): void {
+
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
 }
