f81490a492f565aa338c45caba781bcdba769616
Yevhen_H <yevhen@darly.solutions>
2020-01-25 14:00:06 +0200

    refactored css to scss

--- src/app/components/sub/sub.component.ts
+++ src/app/components/sub/sub.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sub',
   templateUrl: './sub.component.html',
-  styleUrls: ['./sub.component.css']
+  styleUrls: ['./sub.component.scss']
 })
 export class SubComponent implements OnInit {
 
