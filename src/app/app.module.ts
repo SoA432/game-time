e932631fe5f6adbd5d69e0be01415895ee020b56
Yevhen_H <33688931+SoA432@users.noreply.github.com>
2020-01-25 13:53:52 +0200

    initial markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,24 @@ import { GamePlanetComponent } from './components/game-planet/game-planet.compon
 import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ItemComponent } from './components/item/item.component';
+import { SubComponent } from './components/sub/sub.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'blabla',
+    pathMatch: 'full',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +31,15 @@ import { FooterComponent } from './components/footer/footer.component';
     GamePlanetComponent,
     HeaderComponent,
     SliderComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent,
+    ItemComponent,
+    SubComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
