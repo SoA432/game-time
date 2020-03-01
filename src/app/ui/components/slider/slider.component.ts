6750c0b51e68beae8b6e8d99e19a1e450f2ea29d
Yevhen_H <33688931+SoA432@users.noreply.github.com>
2020-03-01 15:49:44 +0200

    Feature/style fixes (#11)
    * WIP
    
    * updated to latest 8 version of angular
    
    * migrated to angular 9
    
    * updated dependencies
    
    * implemented shopping cart, style corrections
    
    * style corrections
    
    Co-authored-by: Yevhen-DarlySolutions <43169692+Yevhen-DarlySolutions@users.noreply.github.com>

--- src/app/ui/components/slider/slider.component.ts
+++ src/app/ui/components/slider/slider.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { GameInterface } from '../../../core/models/game.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slider',
@@ -7,12 +9,30 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
   @Input() gallery: Array<string>;
-  constructor() { }
+  @Input() games: GameInterface[];
+
+  constructor(private router: Router) {
+  }
+
   public sliderInterval = 5000;
   public slides = ['assets/images/diablo3.jpg', 'assets/images/ori.jpg', 'assets/images/witcher3.png'];
 
   ngOnInit() {
-    if (this.gallery) { this.slides = this.gallery; }
+    if (this.gallery) {
+      this.slides = this.gallery;
+    }
+    if (this.games) {
+      this.slides = this.games.map((game: GameInterface) => game.imgSrc);
+    }
+
+    console.log(this.slides);
+  }
+
+  public openGame(imgSrc) {
+    if (this.games) {
+      const slideGame = this.games.find((game: GameInterface) => game.imgSrc === imgSrc);
+      this.router.navigate([`/detail/${slideGame.id}`]);
+    }
   }
 
 }
